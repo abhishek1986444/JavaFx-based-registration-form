eb501530217f1da85b6329770b8e4fde0fc5fa5c
Abhishek <179099403+abhishek1986444@users.noreply.github.com>
2025-12-23 12:17:44 +0530

    added registration function in controller.java ; but main function still here for testing purpose.

--- src/main/java/com/example/controller.java
+++ src/main/java/com/example/controller.java
@@ -1,11 +1,20 @@
 package com.example;
 
+import java.sql.SQLException;
+
+import java.sql.*;
+
+import java.sql.PreparedStatement; 
+
+
+
+@SuppressWarnings(  "unused" )
 public class controller {
 
     private struct object ;
 
 
-public controller () throws Exception{
+public  controller () throws Exception{
 
     DBconnection connection = new DBconnection() ;
 
@@ -13,18 +22,58 @@ public controller () throws Exception{
 
 
 
-    System.out.println("Tablename: "  + object.tablename);
-
-
 }
  
-public static void main ( String [] args ) throws Exception 
+
+
+public  void  register() throws SQLException  // add register function in the controller.java file 
+
+ 
 {
 
-    new controller(); // this for testing part 
-    
+
+String userinput = " insert into userrecord (userid , mailid , password, username ) values ( ?, ? , ?  , ? )  ;   " ; 
+
+PreparedStatement ps = object.connection.prepareStatement(userinput);
+
+
+String  user_id = "" , mail_id = "" , password = "" , username = "" ;
+
+
+
+
+
+ps.setString( 1,user_id );
+ps.setString( 2,mail_id);
+
+ps.setString( 3, password);
+
+ps.setString( 4, username );
+
+
+System.out.println(ps.executeUpdate());
+
+ps.close();
+
+object.connection.close();
+
+
+
+}
+
+
+public  void main ( String [] arg ) throws SQLException
+{
+
+    register();
+
+    System.out.println("function is executed succesfully !! ");
+
 
 }
 
     
 }
+
+
+
